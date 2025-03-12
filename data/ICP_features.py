e198f70314c2e829b2f5f7f18abb48d93322d2b2
Marc777777777777 <m7rc.k7sp7r@gmail.com>
2025-03-12 17:54:41 +0100

    Ajout temps et fix bug etc...

---
+++ data/ICP_features.py
@@ -0,0 +1,194 @@
+ # Import numpy package and name it "np"
+import numpy as np
+
+# Import functions from scikit-learn
+from sklearn.neighbors import KDTree
+from ply import write_ply, read_ply
+
+def compute_local_PCA(query_points, cloud_points, radius):
+    # This function needs to compute PCA on the neighborhoods of all query_points in cloud_points
+
+    tree = KDTree(cloud_points, 40)
+    neighbors_list = tree.query_radius(query_points, radius)
+    # neighbors_list = tree.query(query_points, k=30)   # KNN VERSION
+
+    all_eigenvalues = np.zeros((query_points.shape[0], 3))
+    all_eigenvectors = np.zeros((query_points.shape[0], 3, 3))
+
+    for i in range(neighbors_list.shape[0]):
+        # We first get the list of neighbor indices
+        neighbor_index = neighbors_list[i]
+        n_neighbor = neighbor_index.shape[0]
+        neighbors = cloud_points[neighbor_index, :]
+
+        # Then we compute the PCA and get the eigenvalues and eigenvectors
+        barycenter_neighbor = np.mean(neighbors, axis=0)
+        cov_mat = (1/n_neighbor)*(neighbors-barycenter_neighbor).T@(neighbors-barycenter_neighbor)
+        output_eigh = np.linalg.eigh(cov_mat)
+        eigenvalue = output_eigh[0]
+        eigenvector = output_eigh[1]
+
+        all_eigenvalues[i] = eigenvalue
+        all_eigenvectors[i] = eigenvector
+
+    return all_eigenvalues, all_eigenvectors
+    
+
+def compute_features(query_points, cloud_points, radius):
+    """
+    This function will compute all important geometrical features for each points of query_points.
+    """
+    all_eigenvalues, all_eigenvectors = compute_local_PCA(query_points, cloud_points, radius)
+
+    # normals = all_eigenvectors[:, :, 0]
+    
+    a1D, a2D, a3D = compute_aiD(all_eigenvalues)
+
+    aD = np.column_stack((a1D,a2D,a3D))
+    standard_deviation = np.sqrt(np.maximum(all_eigenvalues, 0))
+    
+    d_star = np.argmax(aD,axis = 1) + 1
+
+    V = standard_deviation[:,0] * standard_deviation[:,1] * standard_deviation[:,2]
+
+    Ef = Shannon_Entropy(a1D, a2D, a3D) 
+
+    return all_eigenvalues, all_eigenvectors, aD, d_star, radius, Ef, V
+    
+
+def compute_aiD(eigenvalues, eps = 1e-6):
+    standard_deviation = np.sqrt(np.maximum(eigenvalues, 0))
+    a1D = standard_deviation[:,2] - standard_deviation[:,1]
+    a2D = standard_deviation[:,1] - standard_deviation[:,0]
+    a3D = standard_deviation[:,0]
+
+    # a1D = 1 - standard_deviation[:,1]/(standard_deviation[:,2]+eps)
+    # a2D = (standard_deviation[:,1] - standard_deviation[:,0])/(standard_deviation[:,2]+eps)
+    # a3D = standard_deviation[:,0]/(standard_deviation[:,2]+eps)
+    
+    # normalization = a1D + a2D + a3D
+    normalization = standard_deviation[:,2]
+    
+    a1D *= 1/normalization
+    a2D *= 1/normalization
+    a3D *= 1/normalization
+
+    return a1D, a2D, a3D
+
+def Shannon_Entropy(a1D, a2D, a3D):
+    """
+    This function computes and returns the Shannon Entropy for a1D, a2D, a3D
+    """
+    return -a1D * np.log(a1D + 1e-10) -a2D * np.log(a2D + 1e-10) -a3D * np.log(a3D + 1e-10) 
+
+def compute_optimal_radius(query_points, point_cloud, radius_list):
+    """
+    For each point of query_points we compute its optimal radius in radius_list.
+    We return optimal_radius_list, an array containing for query_point[i] the optimal radius.
+    """
+    tree = KDTree(point_cloud)
+    n = query_points.shape[0]
+    optimal_radius_list = radius_list[0]*np.ones(n)
+    entropy_list = float("inf")*np.ones(n)
+    for r in radius_list:
+        print("r =",r)
+        neighborhoods = tree.query_radius(query_points, r)
+        for i in range(n):
+            neighbors_idx = neighborhoods[i]
+            n_neighbor = len(neighbors_idx)
+
+            # Skip small neighborhoods
+            if n_neighbor < 10:
+                continue  
+
+            # Compute the PCA and get eigenvalues
+            neighbors = point_cloud[neighbors_idx]
+            # barycenter_neighbor = np.mean(neighbors, axis=0)
+            # cov_mat = (1/n_neighbor)*(neighbors-barycenter_neighbor).T@(neighbors-barycenter_neighbor)
+            cov_mat = np.cov(neighbors.T)
+            eigenvalue = np.linalg.eigh(cov_mat)[0]
+            a1D, a2D, a3D = compute_aiD(eigenvalue.reshape(1,3))
+            entropy = Shannon_Entropy(a1D, a2D, a3D)[0]
+            if entropy < entropy_list[i]:
+                optimal_radius_list[i] = r
+                entropy_list[i] = entropy
+
+
+    return optimal_radius_list
+
+
+def best_rigid_transform(data, ref):
+    '''
+    Computes the least-squares best-fit transform that maps corresponding points data to ref.
+    Inputs :
+        data = (d x N) matrix where "N" is the number of points and "d" the dimension
+         ref = (d x N) matrix where "N" is the number of points and "d" the dimension
+    Returns :
+           R = (d x d) rotation matrix
+           T = (d x 1) translation vector
+           Such that R * data + T is aligned on ref
+    '''
+
+    R = np.eye(data.shape[0])
+    T = np.zeros((data.shape[0],1))
+    data_barycenter = np.mean(data, axis = 1, keepdims = True)
+    ref_barycenter = np.mean(ref, axis = 1, keepdims = True)
+
+    Q_data = data -  data_barycenter
+    Q_ref = ref - ref_barycenter
+
+    H = Q_data@Q_ref.T
+    U, _, Vh = np.linalg.svd(H)
+    R = Vh.T @ U.T
+
+    if np.linalg.det(R) < 0:
+        U[:, -1] *= -1
+        R = Vh.T @ U.T
+
+    T = ref_barycenter - R @ data_barycenter
+    return R, T
+
+
+def best_rigid_transform_weighted(data, ref, weights):
+    '''
+    Computes the least-squares best-fit transform that maps corresponding points data to ref by taking account of the weights.
+    Inputs :
+        data = (d x N) matrix where "N" is the number of points and "d" the dimension
+         ref = (d x N) matrix where "N" is the number of points and "d" the dimension
+         weights   = (N) array where "N" is the number of points
+    Returns :
+           R = (d x d) rotation matrix
+           T = (d x 1) translation vector
+           Such that R * data + T is aligned on ref
+    '''
+
+    R = np.eye(data.shape[0])
+    T = np.zeros((data.shape[0],1))
+    data_barycenter = ((data @ weights) / np.sum(weights)).reshape(3, 1)
+    ref_barycenter = ((ref @ weights) / np.sum(weights)).reshape(3, 1)
+
+    Q_data = data -  data_barycenter
+    Q_ref = ref - ref_barycenter
+
+    W = np.diag(weights)
+
+    H = Q_data@W@Q_ref.T
+
+    U, _, Vh = np.linalg.svd(H)
+
+
+    #There is a variation with another matrix in the middle
+    R = Vh.T @ U.T
+
+    if np.linalg.det(R) < 0:
+        U[:, -1] *= -1
+        R = Vh.T @ U.T
+
+    
+    T = ref_barycenter - R @ data_barycenter
+    return R, T
+
+def computeRn(cloud, n):
+    tree = KDTree(cloud.T, 40)
+    distance_list, _  = tree.query(cloud.T, n)
+    return np.mean(distance_list, axis = 1)
---
+++ data/main.py
@@ -0,0 +1,140 @@
+# Import numpy package and name it "np"
+import numpy as np
+
+# Import library to plot in python
+from matplotlib import pyplot as plt
+
+# Import functions from scikit-learn
+from sklearn.neighbors import KDTree
+
+# Import functions to read and write ply files
+from ply import write_ply, read_ply
+from visu import show_ICP
+
+import sys
+
+# Import functions from all the differents ICP steps
+from ICP_features import *
+from ICP_selection import *
+from ICP_rejection import *
+from ICP_weighting import *
+from ICP import *
+
+if __name__ == '__main__':
+    # Cloud paths
+    #Street
+    #cloud_o_path = '../data/modified_Lille_street_small.ply'
+    #cloud_r_path = '../data/Lille_street_small.ply'
+    #filename = '../stored_features/street.json'             #to store features 
+
+    #Bunny
+    cloud_o_path = '../data/bunny.ply'
+    cloud_r_path = '../data/modified_bunny.ply'
+    filename = '../stored_features/bunny.json'
+
+	# Load clouds
+    cloud_o_ply = read_ply(cloud_o_path)
+    cloud_r_ply = read_ply(cloud_r_path)
+    cloud_o = np.vstack((cloud_o_ply['x'], cloud_o_ply['y'], cloud_o_ply['z']))
+    cloud_r = np.vstack((cloud_r_ply['x'], cloud_r_ply['y'], cloud_r_ply['z']))
+        
+
+    selecting_function =  NoSelection
+    #selecting_function = lambda data, d_star, Ef: RandomSelection(data,d_star, Ef, 0.1)
+    #selecting_function = lambda data, d_star, Ef: EntropySelection(data,d_star, Ef, 0.6)
+    #selecting_function = lambda data, d_star, Ef: EntropySelection(data,d_star,Ef, 0.7)
+    #selecting_function = DimensionSelection
+
+    weighting_function = ConstantWeighting
+    #weighting_function = OmnivarianceWeighting
+    #weighting_function = NormalWeighting
+
+    rejection_function = NoRejection
+    #rejection_function = lambda data, ref, V_data, V_ref : EuclidianRejection(data,ref,V_data, V_ref, 0.7)
+    #rejection_function = lambda data, ref, V_data, V_ref : OmnivarianceRejection(data,ref,V_data, V_ref, 0.5)
+    #rejection_function = lambda data, ref, V_data, V_ref : OmnivarianceRejection(data,ref,V_data, V_ref, 0.7)
+    #rejection_function = lambda data, ref, V_data, V_ref : OmnivarianceRejection(data,ref,V_data, V_ref, 0.9)
+    #rejection_function = DeviationRejection
+
+    selecting_function_list = [
+        NoSelection,
+        lambda data, d_star, Ef: RandomSelection(data,d_star, Ef, 0.1),
+        lambda data, d_star, Ef: EntropySelection(data,d_star, Ef, 0.6),
+        lambda data, d_star, Ef: EntropySelection(data,d_star,Ef, 0.7),
+        DimensionSelection
+    ]
+
+    weighting_function_list = [
+        ConstantWeighting,
+        OmnivarianceWeighting,
+        NormalWeighting
+    ]
+
+    rejection_function_list = [
+        NoRejection,
+        lambda data, ref, V_data, V_ref : EuclidianRejection(data,ref,V_data, V_ref, 0.7),
+        lambda data, ref, V_data, V_ref : OmnivarianceRejection(data,ref,V_data, V_ref, 0.5),
+        lambda data, ref, V_data, V_ref : OmnivarianceRejection(data,ref,V_data, V_ref, 0.7),
+        lambda data, ref, V_data, V_ref : OmnivarianceRejection(data,ref,V_data, V_ref, 0.9),
+        DeviationRejection
+    ]
+
+    iteration = 30
+    tol = 0
+
+    if True:
+        cloud_r_opt, R_list, T_list, neighbors_list, time_list, distance_list = icp_ultimate(cloud_r, cloud_o, iteration, tol, filename, selecting_function, weighting_function, rejection_function)
+
+        show_ICP(cloud_r, cloud_o, R_list, T_list, neighbors_list)
+
+        # Compute RMS
+        #distances2_before = np.sum(np.power(cloud_r - cloud_o, 2), axis=0)
+        #RMS_before = np.sqrt(np.mean(distances2_before))
+        #distances2_after = np.sum(np.power(cloud_r_opt - cloud_o, 2), axis=0)
+        #RMS_after = np.sqrt(np.mean(distances2_after))
+
+        #print('Average RMS between points :')
+        #print('Before = {:.3f}'.format(RMS_before))
+        #print(' After = {:.3f}'.format(RMS_after))
+
+        plt.plot(range(len(distance_list)), distance_list)
+        plt.xlabel("Iterations")
+        plt.ylabel("Distance")
+        plt.title("Evolution of distances")
+        plt.show()    
+
+    if False:
+        label_list = ["Default", "Random","Ef > 0.6", "Ef > 0.7", "d*=2"]
+        for i,s_function in enumerate(selecting_function_list):
+            cloud_r_opt, R_list, T_list, neighbors_list, time_list, distance_list = icp_ultimate(cloud_r, cloud_o, iteration, tol, filename, s_function, weighting_function, rejection_function)
+            plt.semilogy(time_list, distance_list, label = label_list[i])
+
+        plt.xlabel("Time (s)")
+        plt.ylabel("Distance")
+        plt.title("Evolution of distances")
+        plt.legend()
+        plt.show() 
+
+    if False:
+        label_list = ["Default", "Omnivariance","Normal"]
+        for i,w_function in enumerate(weighting_function_list):
+            cloud_r_opt, R_list, T_list, neighbors_list, time_list, distance_list = icp_ultimate(cloud_r, cloud_o, iteration, tol, filename, selecting_function, w_function, rejection_function)
+            plt.semilogy(time_list, distance_list, label = label_list[i])
+
+        plt.xlabel("Time (s)")
+        plt.ylabel("Distance")
+        plt.title("Evolution of distances")
+        plt.legend()
+        plt.show() 
+
+    if False:
+        label_list = ["Default", "d2^70","2.5 * sigma d", "dv50", "dv70", "dv90"]
+        for i,r_function in enumerate(rejection_function_list):
+            cloud_r_opt, R_list, T_list, neighbors_list, time_list, distance_list = icp_ultimate(cloud_r, cloud_o, iteration, tol, filename, selecting_function, weighting_function, r_function)
+            plt.semilogy(time_list, distance_list, label = label_list[i])
+
+        plt.xlabel("Time (s)")
+        plt.ylabel("Distance")
+        plt.title("Evolution of distances")
+        plt.legend()
+        plt.show() 
